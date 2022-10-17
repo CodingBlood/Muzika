3d666771d0b52c039fb353a4d88849e420327fd0
CodingBlood <kartik.agarwal.192.168@gmail.com>
2022-10-17 13:35:45 +0530

    Configured Bootstrap and installed ejsMate. Debugged boiler-plate.ejs

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const bootstrap = require('bootstrap')
-
+var ejsMate = require('ejs-mate')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -13,7 +12,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.engine('ejs', ejsMate)
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,9 +20,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use(
-    express.static(path.join(__dirname, "node_modules/bootstrap/dist/"))
-);
+// app.use(
+//     express.static(path.join(__dirname, "node_modules/bootstrap/dist/"))
+// );
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
