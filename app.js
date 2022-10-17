92771baca6b369a267906c3ab888ba5a8976f01c
CodingBlood <kartik.agarwal.192.168@gmail.com>
2022-10-17 20:10:38 +0530

    finishing Touches

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.use( express.static( "public" ) );
 app.set('view engine', 'ejs');
 app.engine('ejs', ejsMate)
 app.use(logger('dev'));
